bb775361a4d49ff7fddd9d12c603b1c9208f217c
Craig McNamara <cmcnamara87@gmail.cok>
2014-06-22 13:17:48 +1000

    Add AFNetworking. Hide app window

--- Progress/AppDelegate.m
+++ Progress/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "AFNetworking.h"
 
 @implementation AppDelegate
 
@@ -22,7 +23,12 @@
   
   [self setupMenu];
   
-  [self watchConfigFolder];
+  [self.window orderOut:self];
+  
+//  [self watchConfigFolder];
+  
+
+  
 }
 
 - (void)setupMenu
@@ -42,6 +48,23 @@
   [menu addItem:[NSMenuItem separatorItem]];
   [menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@""];
   self.statusItem.menu = menu;
+  
+  [menu addItem:[NSMenuItem separatorItem]];
+  
+  AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+  [manager GET:@"http://localhost:8888/index.php/me/projects" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    
+    self.projects = responseObject;
+    NSLog(@"got json %@", responseObject);
+    for(id project in responseObject) {
+          NSLog(@"got project %@", project);
+      [self watchProject:project];
+      // Add menubar for each project
+      [menu addItemWithTitle:[project objectForKey:@"name"] action:@selector(openWebApp:) keyEquivalent:@""];
+    }
+  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    NSLog(@"Error: %@", error);
+  }];
 
 }
 
@@ -52,26 +75,26 @@
   [[NSWorkspace sharedWorkspace] openURL:URL];
 }
 
-
-#pragma mark - GCD File watching
-/* http://www.davidhamrick.com/2011/10/13/Monitoring-Files-With-GCD-Being-Edited-With-A-Text-Editor.html */
-
-- (void)watchConfigFolder
-{
-  /* Define variables and create a CFArray object containing
-   CFString objects containing paths to watch.
-   */
-  CFStringRef mypath = CFSTR("/Users/cmcnamara87/Desktop");
-  CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&mypath, 1, NULL);
-  void *callbackInfo = NULL; // could put stream-specific data here.
+- (void)watchProject:(NSDictionary *)project {
+//  NSOrderedSet *watchSet = [goal watchedFiles];
+  
+  
+  NSMutableArray *mPaths = [NSMutableArray array];
+  for (NSDictionary *directory in [project objectForKey:@"directories"]) {
+    [mPaths addObject:[directory objectForKey:@"path"]];
+  }
+  NSArray *paths = [mPaths copy];
+  
+  CFArrayRef pathArray = (__bridge CFArrayRef)paths;
+  
   FSEventStreamRef stream;
   CFAbsoluteTime latency = 3.0; /* Latency in seconds */
-  
+  FSEventStreamContext context = {0, (__bridge void*)project, NULL, NULL, NULL};
   /* Create the stream, passing in a callback */
   stream = FSEventStreamCreate(NULL,
-                               &myCallbackFunction,
-                               callbackInfo,
-                               pathsToWatch,
+                               &projectContentChanged,
+                               &context,
+                               pathArray,
                                kFSEventStreamEventIdSinceNow, /* Or a previous event ID */
                                latency,
                                kFSEventStreamCreateFlagNone /* Flags explained in reference */
@@ -79,9 +102,11 @@
   FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(),         kCFRunLoopDefaultMode);
   
   FSEventStreamStart(stream);
+//  NSValue *streamPointer = [NSValue valueWithPointer:stream];
+//  [activeWatchers setObject:streamPointer forKey:[goal created]];
 }
 
-void myCallbackFunction(
+void projectContentChanged(
                         ConstFSEventStreamRef streamRef,
                         void *clientCallBackInfo,
                         size_t numEvents,
@@ -91,14 +116,74 @@ void myCallbackFunction(
 {
   int i;
   char **paths = eventPaths;
-  
-  // printf("Callback called\n");
-  for (i=0; i<numEvents; i++) {
-    //        int count;
-    /* flags are unsigned long, IDs are uint64_t */
-    printf("Change %llu in %s, flags %u\n", eventIds[i], paths[i], (unsigned int)eventFlags[i]);
-  }
-}
 
+  // Check for a bunch of invalid things
+  BOOL numInvalidEvents = 0;
+  for (i=0; i<numEvents; i++) {
+     NSString *eventPath = [NSString stringWithCString:paths[i] encoding:NSUTF8StringEncoding];
+    if ([eventPath rangeOfString:@".git"].location != NSNotFound) {
+      numInvalidEvents++;
+      continue;
+    }
+    if ([eventPath rangeOfString:@".xcworkspace"].location != NSNotFound) {
+      numInvalidEvents++;
+      continue;
+    }
+    if ([eventPath rangeOfString:@".DS_Store"].location != NSNotFound) {
+      numInvalidEvents++;
+      continue;
+    }
+    NSLog(@"Change %llu in %s, flags %u\n", eventIds[i], paths[i], (unsigned int)eventFlags[i]);
+  }
+
+  if(numInvalidEvents == numEvents) {
+    return;
+  }
+  NSLog(@"-- UPLOADING!");
+  
+  // Get out the goal that had the file changed event
+  NSMutableDictionary *project = (__bridge NSMutableDictionary *)(clientCallBackInfo);
+  
+  // Add progress to each project
+  AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+  NSDictionary *parameters = @{@"foo": @"bar"};
+  NSString *url = [NSString stringWithFormat:@"http://localhost:8888/index.php/me/projects/%@/progress", [project objectForKey:@"id"]];
+  NSLog(@"url %@", url);
+  [manager POST:url parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    NSLog(@"JSON: %@", responseObject);
+  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    NSLog(@"Error: %@", error);
+  }];
+
+  // Get out the weekly progress object for that goal
+//  LHWeeklyProgress *weeklyProgress = [goal progressThisWeek];
+  
+  // Check how long its been since the last activity
+  // I'm doing it in 5 min blocks
+//  NSDate *now = [[NSDate alloc] init];
+//  NSTimeInterval secondsBetween = [now timeIntervalSinceDate:[weeklyProgress updated]];
+//  double minutes = secondsBetween / 60.0;
+//  
+//  if (minutes < kMinutesThreshold) {
+//    [goal willChangeValueForKey:@"percentComplete"];
+//    [goal willChangeValueForKey:@"hoursThisWeek"];
+//    double minutesCompleted = [weeklyProgress minutesCompletedValue] + minutes;
+//    [weeklyProgress setMinutesCompletedValue:minutesCompleted];
+//    NSLog(@"Minutes completed: %g", [weeklyProgress minutesCompletedValue]);
+//    [goal didChangeValueForKey:@"percentComplete"];
+//    [goal didChangeValueForKey:@"hoursThisWeek"];
+//  }
+//  
+//  [weeklyProgress setUpdated:[NSDate date]];
+//  NSError *error;
+//  [[refToSelf managedObjectContext] save:&error];
+//  
+//  if (error) {
+//    NSLog(@"%@", error);
+//  }
+  
+//  NSLog(@"The Goal is %@ minutesCompleted: %@", [goal valueForKey:@"title"], [weeklyProgress minutesCompleted]);;
+  
+}
 
 @end
